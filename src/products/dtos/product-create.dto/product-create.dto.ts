157940045fc53fa105c64614da2b2893d9ac0978
glopezhe97 <gerlohe97@gmail.com>
2025-06-16 18:22:36 +0200

    createProduct doesn't work

--- src/products/dtos/product-create.dto/product-create.dto.ts
+++ src/products/dtos/product-create.dto/product-create.dto.ts
@@ -1,1 +1,40 @@
-export class ProductCreateDto {}
+import {
+  IsBoolean,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+} from 'class-validator';
+export class ProductCreateDto {
+  @IsString()
+  name: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsBoolean()
+  @IsOptional()
+  isOnSale?: boolean;
+
+  @IsInt()
+  @IsPositive()
+  stock: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  img_url: string;
+
+  @IsString()
+  brand_name: string;
+
+  @IsString()
+  category_name: string;
+
+  @IsString()
+  subcategory_name: string;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { ProductResponseDto } from './dtos/product-response.dto/product-response.dto';
+import { ProductCreateDto } from './dtos/product-create.dto/product-create.dto';
+import { Brand } from 'src/brands/entities/brands.entity';
+import { Category } from 'src/categories/entities/categories.entity';
+import { Subcategory } from 'src/subcategories/entities/subcategories.entity';
+import { ProductCategorySubcategory } from 'src/productcategorysubcategory/entities/product-category-subcategory.entity';
 @Injectable()
 export class ProductsService {
   constructor(
     @InjectRepository(Product)
     private productRepository: Repository<Product>,
+    @InjectRepository(Brand)
+    private brandRepository: Repository<Brand>,
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+    @InjectRepository(Subcategory)
+    private subcategoryRepository: Repository<Subcategory>,
+    @InjectRepository(ProductCategorySubcategory)
+    private productCategorySubcategoryRepository: Repository<ProductCategorySubcategory>,
   ) {}
 
   // Get all products
@@ -42,7 +55,9 @@ export class ProductsService {
         'subcategory.name AS subcategory_name',
       ]);
     if (name.length > 0) {
-      query.where('product.name ILIKE :name', { name: `%${name}%` });
+      query.where('LOWER(product.name) LIKE :name', {
+        name: `%${name.toLowerCase()}%`,
+      });
     }
     const rawProducts = await query.getRawMany();
     return rawProducts.map((product: ProductResponseDto) => ({
@@ -56,6 +71,50 @@ export class ProductsService {
       category_name: product.category_name,
       subcategory_name: product.subcategory_name,
     }));
+  }
+
+  async createProduct(productCreateDto: ProductCreateDto): Promise<Product> {
+    try {
+      // Search brand ID by name
+      const brand = await this.brandRepository.findOne({
+        where: { name: productCreateDto.brand_name },
+      });
+      if (!brand) throw new NotFoundException('Brand not found');
+
+      console.log(brand);
+      // Search Category
+      const category = await this.categoryRepository.findOne({
+        where: { name: productCreateDto.category_name },
+      });
+      console.log(category);
+      // Search Subcategory
+      const subcategory = await this.subcategoryRepository.findOne({
+        where: { name: productCreateDto.subcategory_name },
+      });
+      console.log(subcategory);
+      // Create product
+      const newProduct: Product = this.productRepository.create({
+        name: productCreateDto.name,
+        price: productCreateDto.price,
+        isOnSale: productCreateDto.isOnSale ?? false,
+        stock: productCreateDto.stock,
+        description: productCreateDto.description ?? '',
+        img_url: productCreateDto.img_url,
+        brand,
+      });
+      //Save product at DDBB
+      const saveNewProduct = await this.productRepository.save(newProduct);
+      // Insert productCategorySubcategory
+      await this.productCategorySubcategoryRepository.save({
+        product_id: saveNewProduct.product_id,
+        category_id: category.category_id,
+        subcategory_id: subcategory.subcategory_id,
+      });
+      return saveNewProduct;
+    } catch (err) {
+      console.log(err.message);
+      return err;
+    }
   }
 }
 // async getProducts(name: string = ''): Promise<ProductResponseDto[]> {
